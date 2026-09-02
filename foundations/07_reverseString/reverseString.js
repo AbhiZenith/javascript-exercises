08de15f19fbdd21e5fceb5a96358f440826f36a2
Abhishek Choudhery <264179459+AbhiZenith@users.noreply.github.com>
2026-09-02 12:52:12 +0300

    feat: complete reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let rev = ""
+    for (let i = string.length - 1; i >= 0; i--) {
+        rev += string[i];
+    }
+    return rev;
 
 };
 
