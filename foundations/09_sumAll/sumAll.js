e92761864c09aa111668599e476ea932b0063ad4
Abhishek Choudhery <264179459+AbhiZenith@users.noreply.github.com>
2026-09-02 15:19:49 +0300

    feat: complete sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+    if (int1 > 0 && int2 > 0 && Number.isInteger(int1) && Number.isInteger(int2)) {
+        const n = Math.max(int1, int2);
+        let total = 0;
 
-};
+        for (let i = Math.min(int1, int2); i <= n; i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        return "ERROR";
+    }
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
