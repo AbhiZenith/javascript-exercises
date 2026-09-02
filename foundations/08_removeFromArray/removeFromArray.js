0ab9756980774df82131feedde53b373c955a387
Abhishek Choudhery <264179459+AbhiZenith@users.noreply.github.com>
2026-09-02 14:53:20 +0300

    feat: complete removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  args.forEach(num => {
+    let index = arr.indexOf(num);
+    
+
+    while (index > -1) {
+      arr.splice(index, 1);
+      index = arr.indexOf(num); 
+    }
+  });
+  
+  return arr;
 };
+
+
+// const removeFromArray = function (arr, ...args) {
+//     args.forEach(num => {
+//         arr.forEach(item => {
+//             const index = arr.indexOf(num)
+//             if (index > -1) arr.splice(index, 1)
+
+//         });
+//     });
+//     return arr;
+// };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
